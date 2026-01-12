69a0381b3454ccf2375db9a7d84c759b4057b636
jesus <theaminishere@gmail.com>
2026-01-12 07:11:01 +0530

    block writer and reader

--- disk-gen.c
+++ disk-gen.c
@@ -1,22 +1,49 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define DISK_SIZE (1024 * 1024)
+#define BLOCK_SIZE 4096
+#define BLOCK_COUNT (DISK_SIZE / BLOCK_SIZE)
+
+void dread(FILE *disk, int BLOCK_NO, void *buff) {
+  fseek(disk, BLOCK_NO * BLOCK_SIZE, SEEK_SET);
+  fread(buff, BLOCK_SIZE, 1, disk);
+}
+
+void dwrite(FILE *disk, int BLOCK_NO, const void *buff) {
+  fseek(disk, BLOCK_NO * BLOCK_SIZE, SEEK_SET);
+  fwrite(buff, BLOCK_SIZE, 1, disk);
+  fflush(disk);
+};
 
 int main() {
+
   FILE *the_disk_file;
-  the_disk_file = fopen("disk.img", "wb");
+  the_disk_file = fopen("disk.img", "w+b");
 
   if (!the_disk_file) {
     perror("file didnt open");
     return 1;
   }
 
-  char nill = 0;
+  char *A = malloc(BLOCK_SIZE);
+  char *B = malloc(BLOCK_SIZE);
 
-  for (int i = 0; i < DISK_SIZE; i++) {
-    fwrite(&nill, 1, 1, the_disk_file);
+  memset(A, 'f', BLOCK_SIZE);
+  memset(B, 'a', BLOCK_SIZE);
+
+  for (int i = 0; i < BLOCK_COUNT; i++) {
+    if (i % 2 == 0) {
+      dwrite(the_disk_file, i, A);
+    } else {
+      dwrite(the_disk_file, i, B);
+    }
   }
+
+  free(A);
+  free(B);
 
   fclose(the_disk_file);
 
